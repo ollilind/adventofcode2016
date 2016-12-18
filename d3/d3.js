01d44e93c477020cf3f6047785c93f104f3ee0d1
Olli Lindroos <olli.lindroos@solita.fi>
2016-12-18 17:43:56 +0200

    d3p1 done

--- d3/d3.js
+++ d3/d3.js
@@ -3,17 +3,30 @@
 let fs = require('fs');
 
 let input = fs.readFile('input', 'utf8', function(err, data) {
-        if(err) {
+    if(err) {
         console.log(err);
         process.exit(1);
-        }
-        d3(data);
-        });
+    }
+    d3(data);
+});
 
 function d3 (input, callback) {
 
-let array = input.split("\n");
+    let array = input.split("\n");
+    array.pop();
+    let correct = 0;
+    
+    array.forEach(function(element,index,array) { 
+        let sides = element.split(/\s+/); 
+        
+        let a = parseInt(sides[0]);
+        let b = parseInt(sides[1]);
+        let c = parseInt(sides[2]);
 
-console.log(array[0].split(/\s+/));
- 
+        if(a < b + c && b < a + c && c < a + b) {
+            correct += 1;
+           }
+    });
+
+    console.log("D3 Part 1: " + correct);
 }; 
