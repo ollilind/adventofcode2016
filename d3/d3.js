67c75a243bd907f175cb71bc194ea97f371c5e6b
Olli Lindroos <olli.lindroos@solita.fi>
2016-12-18 16:30:39 +0200

    regex working

--- d3/d3.js
+++ d3/d3.js
@@ -13,7 +13,7 @@ let input = fs.readFile('input', 'utf8', function(err, data) {
 function d3 (input, callback) {
 
 let array = input.split("\n");
-console.log(array);
-console.log(array[0].split(/(\s+)/));
-    
+
+console.log(array[0].split(/\s+/));
+ 
 }; 
